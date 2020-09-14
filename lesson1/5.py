5d308023f421d0782ba6978506ac742e68ab9a2c
Anna Filippova <anna.filipp@rambler.ru>
2020-09-14 13:18:14 +0300

    Revert "Remove files"
    This reverts commit b91e68c2dfecf2dd18d1b3550ca01d005d0e1240.

---
+++ lesson1/5.py
@@ -0,0 +1,10 @@
+#словари
+
+a= {"city": "Москва", "temperature": 20}
+print(a["city"])
+print(a)
+a["temperature"] -= 5
+print(a)
+print(a.get("country", "Россия"))
+a["date"] = "27.05.2019"
+print (len(a))
