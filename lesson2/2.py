0b1c2cea6be43d9c18d70a56f2e04336ccbf88be
Anna Filippova <anna.filipp@rambler.ru>
2020-09-15 22:23:01 +0300

    task_1

---
+++ lesson2/2.py
@@ -0,0 +1,23 @@
+
+def string(s1, s2):
+
+    if type(s1) is not str or type(s2) is not str:
+        return 0
+    elif type(s1) or type(s2) is str:
+        if len(s1) == len(s2):
+            return 1
+        elif len(s1) > len(s2):
+            return 2
+        elif len(s1) < len(s2):
+            return 3
+    else:
+        raise ValueError('Какая то хня')
+
+print(string(1, "1"))
+print(string(1, True))
+print(string(False, "1"))
+print(string("True", "1"))
+print(string(1, 1))
+print(string("Hello", "Hi"))
+print(string("Hello", "hello"))
+print(string("Hi", "Hello"))
